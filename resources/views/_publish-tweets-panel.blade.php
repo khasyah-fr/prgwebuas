143b839bfef4ce80f11b76bd3bc558b8a9700f58
Vincent Gunawan <gnwnvincent@gmail.com>
2020-12-03 14:09:39 +0700

    Install Laravel and set up see following tweets

---
+++ resources/views/_publish-tweets-panel.blade.php
@@ -0,0 +1,18 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
+    <form method="POST" action="/tweets">
+        @csrf
+
+        <textarea name="body" id="" class="w-full" placeholder="What's going on?"></textarea>
+
+        <hr class="my-4">
+
+        <footer class="flex justify-between">
+            <img src="{{ auth()->user()->avatar }}" alt="user-avatar" class="rounded-full mr-4">
+            <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweet-mee!!</button>
+        </footer>
+    </form>
+
+    @error('body')
+        <p class="text-red-600 text-sm mt-2">{{ $message }}</p>
+    @enderror
+</div>
