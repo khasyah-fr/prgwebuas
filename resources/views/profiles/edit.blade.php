5acae75eec81ce4c447b9bee6af76b4127424565
Vincent Gunawan <gnwnvincent@gmail.com>
2020-12-04 15:46:26 +0700

    Edit some css stuff

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -5,7 +5,7 @@
 
         <div class="mb-6">
             <label for="name" class="mb-2 uppercase block font-bold text-gray-700 text-xs">Name</label>
-            <input type="text" class="border border-gray-400 p-2 w-full" name="name" id="name" required value="{{ $user->name }}">
+            <input type="text" class="border border-gray-400 p-2 w-full rounded-2xl" name="name" id="name" required value="{{ $user->name }}">
 
             @error('name')
                 <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
@@ -15,7 +15,7 @@
         <div class="mb-6">
             <label for="avatar" class="mb-2 uppercase block font-bold text-gray-700 text-xs">Avatar</label>
             <div class="flex">
-                <input type="file" class="border border-gray-400 p-2 w-full" name="avatar" id="avatar" required>
+                <input type="file" class="border border-gray-400 p-2 w-full rounded-2xl" name="avatar" id="avatar">
                 <img class="ml-2" src="{{ $user->avatar }}" alt="" width=50>
             </div>
             @error('avatar')
@@ -26,7 +26,7 @@
 
         <div class="mb-6">
             <label for="username" class="mb-2 uppercase block font-bold text-gray-700 text-xs">Username</label>
-            <input type="text" class="border border-gray-400 p-2 w-full" name="username" id="username" value="{{ $user->username }}" required>
+            <input type="text" class="border border-gray-400 p-2 w-full rounded-2xl" name="username" id="username" value="{{ $user->username }}" required>
 
             @error('username')
             <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
@@ -35,7 +35,7 @@
 
         <div class="mb-6">
             <label for="email" class="mb-2 uppercase block font-bold text-gray-700 text-xs">Email</label>
-            <input type="email" class="border border-gray-400 p-2 w-full" name="email" id="email" value="{{ $user->email }}" required>
+            <input type="email" class="border border-gray-400 p-2 w-full rounded-2xl" name="email" id="email" value="{{ $user->email }}" required>
 
             @error('email')
             <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
@@ -44,7 +44,7 @@
 
         <div class="mb-6">
             <label for="password" class="mb-2 uppercase block font-bold text-gray-700 text-xs">Password</label>
-            <input type="password" class="border border-gray-400 p-2 w-full" name="password" id="password" required>
+            <input type="password" class="border border-gray-400 p-2 w-full rounded-2xl" name="password" id="password" required>
 
             @error('password')
             <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
@@ -53,7 +53,7 @@
 
         <div class="mb-6">
             <label for="password_confirmation" class="mb-2 uppercase block font-bold text-gray-700 text-xs">Password Confirmation</label>
-            <input type="password" class="border border-gray-400 p-2 w-full" name="password_confirmation" id="password_confirmation" required>
+            <input type="password" class="border border-gray-400 p-2 w-full rounded-2xl" name="password_confirmation" id="password_confirmation" required>
 
             @error('password_confirmation')
             <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
